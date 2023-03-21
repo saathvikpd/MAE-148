b7cc129a3caf27e297415decb52a497756429ac1
saathvikpd <saathvik.pd@gmail.com>
2023-03-20 17:59:47 -0700

    steering angle -TK

--- depthai-script-cv.py
+++ depthai-script-cv.py
@@ -30,12 +30,40 @@ if __name__ == '__main__':
         #frame - frame after preprocs, with predictions
         #raw_frame - original frame from your OAK
         #depth - depth map for raw_frame, center-rectified to the center camera
-
+        
+        preds = [p.json() for p in predictions]
+        
+        bottom = (frame.shape[0] // 2, frame.shape[1])
+        
+        def dist(p1, p2):
+            return (((p1[0] - p2[0])**2) + ((p1[1] - p2[1])**2))**0.5
+        
+        mean_angle = 0
+        if len(preds) >= 1:
+            for p in preds:
+                x = p['x']
+                y = p['y']
+                
+                cv2.line(frame, (frame.shape[0] // 2, frame.shape[1]), (int(x), int(y)), color = (0, 0, 0), thickness = 1)
+                
+                adj = frame.shape[1] - y
+                hyp = dist((x, y), bottom)
+                cos = adj / hyp
+                angle = np.arccos(cos)
+                if x < bottom[0]:
+                    angle = -1 * angle 
+                mean_angle += angle
+            mean_angle = mean_angle / len(preds)
+        
         # timing: for benchmarking purposes
         t = time.time()-t0
         print("INFERENCE TIME IN MS ", 1/t)
-        print("PREDICTIONS ", [p.json() for p in predictions])
-
+        print("PREDICTIONS ", preds)
+        
+        cv2.line(frame, (frame.shape[0] // 2, frame.shape[1]), (frame.shape[0] // 2, 0), color = (0, 0, 0), thickness = 1)
+        
+        cv2.putText(frame, str(mean_angle * 180 / np.pi) + " degrees", org = (0, 25), color = (0, 0, 0), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 0.7, thickness = 1)
+        
         # setting parameters for depth calculation
         max_depth = np.amax(depth)
         cv2.imshow("depth", depth/max_depth)
