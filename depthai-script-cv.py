ed9251bf000c1967311c27cb79c8617d73de7100
saathvikpd <saathvikpd@users.noreply.github.com>
2023-03-22 16:02:10 -0700

    Update depthai-script-cv.py

--- depthai-script-cv.py
+++ depthai-script-cv.py
@@ -2,7 +2,33 @@ from roboflowoak import RoboflowOak
 import cv2
 import time
 import numpy as np
-from vesc_control import steer
+from pyvesc import VESC
+
+def steer(angle):
+    # serial port that VESC is connected to. Something like "COM3" for windows and as below for linux/mac
+    serial_port = '/dev/ttyACM0'
+
+    motor = VESC(serial_port=serial_port)
+    print("Firmware: ", motor.get_firmware_version())
+
+    # sweep servo through full range
+#     for i in range(100):
+#         time.sleep(0.01)
+#         motor.set_servo(i/100)
+    steer_input = (angle + 85) / 170
+    if steer_input < 0:
+        steer_input = 0
+    if steer_input > 1:
+        steer_input = 1
+        
+    motor.set_servo(steer_input)
+
+    # IMPORTANT: YOU MUST STOP THE HEARTBEAT IF IT IS RUNNING BEFORE IT GOES OUT OF SCOPE. Otherwise, it will not
+    #            clean-up properly.
+    motor.stop_heartbeat()
+    
+def throttle(rpm):
+    return None
 
 if __name__ == '__main__':
     # instantiating an object (rf) with the RoboflowOak module
