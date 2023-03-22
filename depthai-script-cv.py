680e7df1b72108e15c1b500021b9fd4de712a347
saathvikpd <saathvikpd@users.noreply.github.com>
2023-03-22 16:59:21 -0700

    Update depthai-script-cv.py

--- depthai-script-cv.py
+++ depthai-script-cv.py
@@ -96,12 +96,12 @@ while True:
     #     time.sleep(0.01)
         motor.set_servo(steer_input)
         
-        motor.set_rpm(500)
+        motor.set_duty_cycle(.02)
         for i in range(30):
             time.sleep(0.1)
-#             print(motor.get_measurements().rpm)
+            print(motor.get_measurements().rpm)
         motor.set_rpm(0)
-#         motor.set_duty_cycle(.02)
+#         
     # run motor and print out rpm for ~2 seconds
     
    
