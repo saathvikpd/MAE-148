c60079a3be5e69f20e455bd8cb80dd8e9455c580
saathvikpd <saathvikpd@users.noreply.github.com>
2023-03-22 17:55:19 -0700

    Update depthai-script-cv.py

--- depthai-script-cv.py
+++ depthai-script-cv.py
@@ -118,8 +118,9 @@ for _ in range(1000):
             motor.set_rpm(0)
         except:
             continue
-        os.system("$S")
-        motor = VESC(serial_port=serial_port)
+         
+    os.system("$S")
+    motor = VESC(serial_port=serial_port)
         
 #         motor.set_duty_cycle(
 #         motor.set_rpm(100)
